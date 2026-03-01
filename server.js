4d9b71258d6c5abb4c0d97b8a77dc522fea7f3e6
maXik <mftank22@gmail.com>
2026-03-01 20:07:33 +0200

    init

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors')
+const mongoose = require('mongoose');
+const User = require('./User')
+
+const app = express();
+
+app.use(cors({
+	origin: ['http://127.0.0.1:5500', 'http://localhost:5500']
+}));
+app.use(express.json())
+
+mongoose.connect('mongodb+srv://maxi4okdev_db_user:<db_password>@users.vbsgbag.mongodb.net/')
+	.then(() => { console.log('connected') })
+	.catch(e => console.error(e))
+
+
+app.get('/', async (req, res) => {
+	try {
+		const users = await User.find({});
+		res.status(200).json(users);
+	} catch (error) {
+		res.status(500).json({ message: "Cant access data" });
+	}
+})
+app.post('/', async (req, res) => {
+	const newUser = await User.create({
+		name: req.body.name,
+		age: req.body.age
+	});
+
+	console.log('New user:', newUser.name);
+
+	res.status(201).json(newUser);
+});
+app.delete('/', async (req,res) => {
+	await User.deleteMany({});
+	res.status(200).json('Deleted!');
+})
+
+
+app.listen(3000)
